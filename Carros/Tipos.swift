a18f1a639ccb9e8c53db34d20ea9d75b9f0b6dae
Marcelo Block <marcelochb@gmail.com>
2020-07-03 22:34:35 -0300

    feat(started) Cadastro de usuarios e carros

---
+++ Carros/Tipos.swift
@@ -0,0 +1,21 @@
+//
+//  Tipos.swift
+//  Carros
+//
+//  Created by Marcelo Block Teixeira on 03/07/20.
+//  Copyright © 2020 Marcelo Block Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class Tipos: UIPickerView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
